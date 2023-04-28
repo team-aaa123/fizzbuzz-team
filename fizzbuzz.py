c7029ff184e9ea345a84ae1829ada89e30c21721
Jin_00 <anjdal64@gmail.com>
2023-04-28 20:58:26 +0900

    feat : fizzbuzz function if fizz : 3 then Print fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,12 @@
 def do_fizzbuzz(num: int):
+    for i in range(1, num+1):
+        if i % 3 == 0:
+            print('fizz')
+        else:
+            print(i)
     print(f'{num} hello')
 
 
 if __name__ == '__main__':
-    do_fizzbuzz(1)
+    do_fizzbuzz(16)
     
